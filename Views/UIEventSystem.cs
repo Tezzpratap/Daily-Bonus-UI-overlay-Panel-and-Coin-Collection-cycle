a75cc1e154ff73b8f1dd8171c263de78f30e2c7c
Tezz pratap <99133377+Tezzpratap@users.noreply.github.com>
2025-02-06 23:18:35 +0530

    Update UIEventSystem.cs

--- Views/UIEventSystem.cs
+++ Views/UIEventSystem.cs
@@ -2,26 +2,25 @@ using System;
 
 public class UIEventSystem
 {
-    public static UIEventSystem Instance { get; } = new UIEventSystem();
+    public static UIEventSystem Instance { get; } = new UIEventSystem(); // Singleton instance
 
-    // Events
-    public event Action<int> OnRewardCollected;
-    public event Action<string> OnPanelOpened;
-    public event Action<string> OnPanelClosed;
+    public event Action<int> OnRewardCollected; // Event to trigger when reward is collected
+    public event Action<string> OnPanelOpened; // Event to trigger when panel is opened
+    public event Action<string> OnPanelClosed; // Event to trigger when panel is closed
 
-    private UIEventSystem() { }
+    private UIEventSystem() { } // Private constructor to prevent instantiation
 
-    public void TriggerRewardCollected(int dayIndex)
+    public void TriggerRewardCollected(int day) // Method to trigger reward collected event
     {
-        OnRewardCollected?.Invoke(dayIndex);
+        OnRewardCollected?.Invoke(day);
     }
 
-    public void TriggerPanelOpened(string panelName)
+    public void TriggerPanelOpened(string panelName) // Method to trigger panel opened event
     {
         OnPanelOpened?.Invoke(panelName);
     }
 
-    public void TriggerPanelClosed(string panelName)
+    public void TriggerPanelClosed(string panelName) // Method to trigger panel closed event
     {
         OnPanelClosed?.Invoke(panelName);
     }

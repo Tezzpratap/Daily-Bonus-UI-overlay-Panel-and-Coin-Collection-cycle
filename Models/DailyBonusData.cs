2e6e79c35f049c6428495f019913b9810f57eaf0
Tezz pratap <99133377+Tezzpratap@users.noreply.github.com>
2025-02-07 12:26:34 +0530

    Update DailyBonusData.cs
    made a random selection for gifts on day 3 and 7

--- Models/DailyBonusData.cs
+++ Models/DailyBonusData.cs
@@ -1,13 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "DailyBonusData", menuName = "ScriptableObjects/DailyBonusData", order = 1)]
+public class DailyBonusData : ScriptableObject
+{
+    //public List<string> dailyBonusRewards; // List for daily bonus rewards
+    public List<int> dailyBonusRewards; // List for daily bonus rewards
 
-public class DailyBonusData : ScriptableObject  
-{  
-    public int[] dailyBonusRewards; // Array for daily bonus rewards  
+    private void OnEnable()
+    {
+        //dailyBonusRewards = new List<int> { 10, 50, 0, 80, 100, 150, 200 };
+        dailyBonusRewards = new List<int>
+        {
+            10,
+            50,
+            0, //GetRandomReward3rdDay(), // Random coins or gift
+            90,
+            100,
+            150,
+            200,//GetRandomReward7thDay(),  // Random coins or gift
+        };
+        //dailyBonusRewards = new List<string> { "10 Coins", "50 Coins", "0 Coins", "80 Coins", "100 Coins", "150 Coins", "200 Coins" };
+        /*dailyBonusRewards = new List<string>
+        {
+            "10 Coins",
+            "50 Coins",
+            GetRandomReward3rdDay(), //Random coins or gift
+            "90 Coins",
+            "100 Coins",
+            "150 Coins",
+            GetRandomReward7thDay(),  // Random coins or gift
+        };*/
+    }
 
-    private void OnEnable() 
-    {  
-        dailyBonusRewards = new int[7] { 10, 50, 0, 80, 100, 150, 200 }; // Set the rewards for each day  
-    }  
+    private string GetRandomReward3rdDay()
+    {
+        int randomRewardForDay3 = Random.Range(50, 100); // Random number between 50 and 200
+        return randomRewardForDay3 + " Coins";
+    }
+
+    private string GetRandomReward7thDay(){
+        int randomRewardForDay7 = Random.Range(150, 250); // Random number between 150 and 250
+        return randomRewardForDay7 + " Coins";
+    }
 }
+
+
+

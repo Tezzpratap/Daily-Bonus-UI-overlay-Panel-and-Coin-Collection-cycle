f16827cb784ce9218c6bc21f81da287c58536c25
Tezz pratap <99133377+Tezzpratap@users.noreply.github.com>
2025-02-08 13:54:48 +0530

    Update UIEventSystem.cs

--- Views/UIEventSystem.cs
+++ Views/UIEventSystem.cs
@@ -1,28 +1,37 @@
 using System;
+using UnityEngine;
 
-public class UIEventSystem
+public class UIEventSystem : MonoBehaviour
 {
-    public static UIEventSystem Instance { get; } = new UIEventSystem();
+    public GameEvent rewardCollectedEvent; // ScriptableObject reference
+    public GameEvent panelOpenedEvent;
+    public GameEvent panelClosedEvent;
 
-    // Events
-    public event Action<int> OnRewardCollected;
-    public event Action<string> OnPanelOpened;
-    public event Action<string> OnPanelClosed;
-
-    private UIEventSystem() { }
-
-    public void TriggerRewardCollected(int dayIndex)
+    /*public void TriggerRewardCollected(int day)
     {
-        OnRewardCollected?.Invoke(dayIndex);
+        rewardCollectedEvent.RaiseEvent();
+    }*/
+
+    public static UIEventSystem Instance { get; } = new UIEventSystem(); // Singleton instance
+
+    public event Action<int> OnRewardCollected; // Event to trigger when reward is collected
+    public event Action<string> OnPanelOpened; // Event to trigger when panel is opened
+    public event Action<string> OnPanelClosed; // Event to trigger when panel is closed
+
+    private UIEventSystem() { } // Private constructor to prevent instantiation
+
+    public void TriggerRewardCollected(int day) // Method to trigger reward collected event
+    {
+        OnRewardCollected?.Invoke(day); // Invoke the event
     }
 
-    public void TriggerPanelOpened(string panelName)
+    public void TriggerPanelOpened(string panelName) // Method to trigger panel opened event
     {
-        OnPanelOpened?.Invoke(panelName);
+        OnPanelOpened?.Invoke(panelName); // Invoke the event
     }
 
-    public void TriggerPanelClosed(string panelName)
+    public void TriggerPanelClosed(string panelName) // Method to trigger panel closed event
     {
-        OnPanelClosed?.Invoke(panelName);
+        OnPanelClosed?.Invoke(panelName); // Invoke the event
     }
 }
